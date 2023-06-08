3ac2a6ff5f98dd8975733c4c958f873be795d81c
Alonzo <zcodes101@gmail.com>
2023-06-08 10:36:07 -0400

    first-commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBddftkGzM9kgSoGyck0a0AC55shJaCv34",
-    authDomain: "pmdashboard-4684f.firebaseapp.com",
-    projectId: "pmdashboard-4684f",
-    storageBucket: "pmdashboard-4684f.appspot.com",
-    messagingSenderId: "56862662739",
-    appId: "1:56862662739:web:f8c3d2255c66cdfad4a6a6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
